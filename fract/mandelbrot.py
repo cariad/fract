d9e3ca03d7203462f1610bb02f63e1b231225dc6
Cariad Eccleston <cariad@cariad.earth>
2023-09-17 11:05:37 +0100

    Add optimisations (#3)

--- fract/mandelbrot.py
+++ fract/mandelbrot.py
@@ -1,88 +1,156 @@
+from datetime import datetime
 from math import ceil
-from multiprocessing import Pool, cpu_count
+from multiprocessing import Pool, Queue, cpu_count, current_process
 from pathlib import Path
 from tempfile import TemporaryDirectory
 from typing import Iterator, Optional
 
 import png
 
-from fract.process_instruction import ProcessInstruction
-from fract.process_planner import ProcessPlanner
-from fract.segment import Segment
-from fract.segment_planner import SegmentPlanner
+from fract.logging import logger
 
 
-def calculate(
-    instructions: Iterator[ProcessInstruction],
+def count_all_iterations(
+    render_width: int,
+    render_height: int,
+    real: float,
+    imaginary: float,
+    real_width: float,
+    max_iterations: int,
     max_processes: int,
+    result_byte_length: int,
+    working_directory: Path,
 ) -> None:
-    with Pool(processes=max_processes) as pool:
-        pool.map_async(
-            calculate_segment,
-            instructions,
-        )
+    queue: "Queue[int]" = Queue()
+    start = datetime.now()
 
+    for y in range(render_height):
+        queue.put(y)
+
+    for _ in range(max_processes):
+        queue.put(-1)
+
+    imaginary_height = real_width * (render_height / render_width)
+
+    worker_args = (
+        render_width,
+        render_height,
+        real_width,
+        real - (real_width / 2),
+        imaginary_height,
+        imaginary - (imaginary_height / 2),
+        max_iterations,
+        queue,
+        result_byte_length,
+        working_directory,
+    )
+
+    logger.debug("Starting a pool of %i processes", max_processes)
+    with Pool(max_processes, start_row_listener, worker_args) as pool:
         pool.close()
+
+        logger.debug("Waiting for workers to finish...")
         pool.join()
 
-
-def calculate_segment(i: ProcessInstruction) -> None:
-    """
-    Generates a temporary iteration count file for a row segment.
-    """
-
-    with open(i["result_path"], "wb") as f:
-        for px in range(i["min_x"], i["max_x"]):
-            viewport_x_percent = px / i["render_width"]
-
-            c = complex(
-                i["plane_min_x"] + viewport_x_percent * i["plane_width"],
-                i["plane_y"],
-            )
-
-            iterations = count_iterations(
-                c,
-                i["max_iterations"],
-            )
-
-            f.write(iterations.to_bytes(i["result_byte_length"]))
+    logger.debug(
+        "Calculated all iterations in %.1f seconds",
+        (datetime.now() - start).total_seconds(),
+    )
 
 
 def count_iterations(
-    c: complex,
+    real: float,
+    imaginary: float,
     maximum: int,
 ) -> int:
     """
-    Counts and returns the iterations for a complex number.
+    Counts the number of iterations required for the point (`real`, `imaginary`)
+    to escape the Mandelbrot set, to a `maximum` iteration.
     """
 
-    count = 0
-    z = c
+    if estimate_in_mandelbrot_set(real, imaginary):
+        return maximum
 
-    while count < maximum and (z * z.conjugate()).real < 4:
-        z = (z * z) + c
+    count = 0
+
+    x = 0.0
+    y = 0.0
+
+    x_squared = 0.0
+    y_squared = 0.0
+
+    x_cycle = 0.0
+    y_cycle = 0.0
+
+    period = 0
+
+    while x_squared + y_squared <= 4.0 and count < maximum:
+        y = ((2 * x) * y) + imaginary
+        x = (x_squared - y_squared) + real
+
+        x_squared = x * x
+        y_squared = y * y
+
+        if x == x_cycle and y == y_cycle:
+            return maximum
+
+        period += 1
+
+        if period > 20:
+            period = 0
+            x_cycle = x
+            y_cycle = y
+
         count += 1
 
     return count
 
 
-def iterations_to_pixels(
+def estimate_in_mandelbrot_set(
+    real: float,
+    imaginary: float,
+) -> bool:
+    """
+    Estimates whether or not the point of (`real`, `imaginary`) is inside the
+    Mandelbrot set's cardioid or a second-order bulb.
+
+    `True` indicates certainty that the point is within the cardioid or a
+    second-order bulb. `False` indicates uncertainty whether the point is inside
+    or outside.
+    """
+
+    # Check cardioid:
+
+    imaginary_squared = imaginary * imaginary
+
+    real_minus_quarter = real - 0.25
+
+    q = (real_minus_quarter * real_minus_quarter) + imaginary_squared
+
+    if q * (q + real_minus_quarter) <= (0.25 * imaginary_squared):
+        return True
+
+    # Check bulbs:
+
+    real_plus_one = real + 1
+
+    return (real_plus_one * real_plus_one) + imaginary_squared <= 0.0625
+
+
+def iterations_to_color_rows(
+    height: int,
     max_iterations: int,
     result_byte_length: int,
-    segments: Iterator[Segment],
+    working_directory: Path,
 ) -> Iterator[list[int]]:
-    current_y: Optional[int] = None
     colors: list[int] = []
 
-    for segment in segments:
-        if current_y != segment["y"]:
-            if current_y is not None:
-                yield colors
-                colors = []
+    for y in range(height):
+        if y > 0:
+            yield colors
+            colors = []
 
-            current_y = segment["y"]
-
-        with open(segment["path"], "rb") as f:
+        with open(working_directory / str(y), "rb") as f:
             while read_bytes := f.read(result_byte_length):
                 iteration_count = int.from_bytes(read_bytes)
 
@@ -94,56 +162,39 @@ def iterations_to_pixels(
     yield colors
 
 
-def render(
+def render_mandelbrot(
     width: int,
     height: int,
-    path: Path,
-    max_iterations: int = 500,
+    path: Path | str,
+    max_iterations: int = 1_000,
     max_processes: Optional[int] = None,
     x: float = -0.65,
     y: float = 0,
     zoom: float = 3.0,
 ) -> None:
     """
-    Renders the Mandelbrot Set to a PNG file.
+    Renders the Mandelbrot set to a PNG file.
     """
 
     max_processes = cpu_count() - 1 if max_processes is None else max_processes
-
-    # Give each thread enough to keep busy for at least a few seconds. If
-    # segments are too small then threads will spend more time starting/stopping
-    # than working. This value works on my laptop, so nyah.
-    max_segment_width = 100_000
-
-    plot_height = zoom * (height / width)
     result_byte_length = ceil(max_iterations.bit_length() / 8.0)
 
     with TemporaryDirectory() as temp_dir:
         working_directory = Path(temp_dir)
 
-        segment_planner = SegmentPlanner(
-            max_segment_width,
-            height,
+        count_all_iterations(
             width,
+            height,
+            x,
+            y,
+            zoom,
+            max_iterations,
+            max_processes,
+            result_byte_length,
             working_directory,
         )
 
-        process_planner = ProcessPlanner(
-            max_iterations,
-            plot_height,
-            zoom,
-            x - (zoom / 2),
-            y - (plot_height / 2),
-            height,
-            width,
-            result_byte_length,
-            segment_planner.make(),
-        )
-
-        calculate(
-            process_planner.make(),
-            max_processes=max_processes,
-        )
+        start = datetime.now()
 
         with open(path, "wb") as f:
             writer = png.Writer(
@@ -152,11 +203,55 @@ def render(
                 greyscale=False,
             )
 
-            writer.write(
-                f,
-                iterations_to_pixels(
-                    max_iterations,
-                    result_byte_length,
-                    segment_planner.make(),
-                ),
+            rows = iterations_to_color_rows(
+                height,
+                max_iterations,
+                result_byte_length,
+                working_directory,
             )
+
+            writer.write(f, rows)
+
+        logger.debug(
+            "Wrote PNG in %.1f seconds",
+            (datetime.now() - start).total_seconds(),
+        )
+
+
+def start_row_listener(
+    render_width: int,
+    render_height: int,
+    real_width: float,
+    real_min: float,
+    imaginary_height: float,
+    imaginary_min: float,
+    max_iterations: int,
+    queue: "Queue[int]",
+    result_byte_length: int,
+    working_directory: Path,
+) -> None:
+    name = current_process().name
+    logger.debug("Worker process %s started", name)
+
+    while True:
+        y = queue.get()
+
+        if y < 0:
+            logger.debug("Worker process %s stopping", name)
+            return
+
+        logger.debug("Worker process %s now working on line %i", name, y)
+
+        imaginary = imaginary_min + (y / render_height) * imaginary_height
+
+        path = working_directory / str(y)
+
+        with open(path, "wb") as f:
+            for x in range(render_width):
+                count = count_iterations(
+                    real_min + (x / render_width) * real_width,
+                    imaginary,
+                    max_iterations,
+                )
+
+                f.write(count.to_bytes(result_byte_length))
--- tests/test_mandelbrot.py
+++ tests/test_mandelbrot.py
@@ -1,97 +1,59 @@
 from pathlib import Path
-from tempfile import NamedTemporaryFile, TemporaryDirectory
 
-from fract import render
-from fract.mandelbrot import calculate_segment, iterations_to_pixels
-from fract.process_instruction import ProcessInstruction
-from fract.segment import Segment
+from pytest import mark
 
-
-def test_calculate_segment() -> None:
-    result_byte_length = 1
-
-    with NamedTemporaryFile() as f:
-        instruction = ProcessInstruction(
-            min_x=23,
-            max_iterations=100,
-            max_x=26,
-            plane_min_x=-1.0,
-            plane_width=2.0,
-            plane_y=0.1,
-            render_width=200,
-            result_byte_length=result_byte_length,
-            result_path=Path(f.name),
-        )
-
-        calculate_segment(instruction)
-
-        f.seek(0)
-
-        iterations: list[int] = []
-
-        with open(f.name, "rb") as fr:
-            while read_bytes := fr.read(result_byte_length):
-                iterations.append(int.from_bytes(read_bytes))
-
-        assert iterations == [100, 30, 32]
+from fract import render_mandelbrot
+from fract.mandelbrot import count_iterations, estimate_in_mandelbrot_set
 
 
-def test_iterations_to_pixels() -> None:
-    max_iterations = 100
-    result_byte_length = 1
+@mark.parametrize(
+    "real, imaginary, expect",
+    [
+        (0, 0, 100),
+        (
+            -0.124999999999999555910790149937383830547332763671875,
+            -0.854999999999999982236431605997495353221893310546875,
+            100,
+        ),
+        (
+            -0.252499999999999946709294817992486059665679931640625,
+            -1.0725000000000000088817841970012523233890533447265625,
+            6,
+        ),
+    ],
+)
+def test_count_iterations(
+    real: float,
+    imaginary: float,
+    expect: int,
+) -> None:
+    assert count_iterations(real, imaginary, 100) == expect
 
-    with TemporaryDirectory() as temp_dir:
-        working_directory = Path(temp_dir)
 
-        s1 = Segment(
-            max_x=1,
-            min_x=3,
-            path=working_directory / "1",
-            y=45,
-        )
-
-        s2 = Segment(
-            max_x=3,
-            min_x=6,
-            path=working_directory / "2",
-            y=45,
-        )
-
-        s3 = Segment(
-            max_x=1,
-            min_x=9,
-            path=working_directory / "3",
-            y=46,
-        )
-
-        with open(s1["path"], "wb") as f:
-            f.write((100).to_bytes(result_byte_length))
-            f.write((99).to_bytes(result_byte_length))
-
-        with open(s2["path"], "wb") as f:
-            f.write((0).to_bytes(result_byte_length))
-            f.write((100).to_bytes(result_byte_length))
-
-        with open(s3["path"], "wb") as f:
-            f.write((0).to_bytes(result_byte_length))
-            f.write((100).to_bytes(result_byte_length))
-            f.write((0).to_bytes(result_byte_length))
-
-        pixels = iterations_to_pixels(
-            max_iterations,
-            result_byte_length,
-            iter([s1, s2, s3]),
-        )
-
-        assert list(pixels) == [
-            [0, 0, 0, 255, 255, 255, 255, 255, 255, 0, 0, 0],
-            [255, 255, 255, 0, 0, 0, 255, 255, 255],
-        ]
+@mark.parametrize(
+    "real, imaginary, expect",
+    [
+        (0, 0, True),
+        (-1, 0, True),
+        (1, 1, False),
+    ],
+)
+def test_estimate_in_mandelbrot_set(
+    real: float,
+    imaginary: float,
+    expect: bool,
+) -> None:
+    assert estimate_in_mandelbrot_set(real, imaginary) is expect
 
 
 def test_render() -> None:
-    render(
-        800,
-        600,
-        Path("docs") / "demo.png",
-    )
+    docs = Path("docs")
+
+    width = 800
+    height = 600
+
+    render_mandelbrot(width, height, docs / "demo.png")
+    render_mandelbrot(width, height, docs / "bottom.png", y=1.4)
+    render_mandelbrot(width, height, docs / "down.png", y=1)
+    render_mandelbrot(width, height, docs / "top.png", y=-1.4)
+    render_mandelbrot(width, height, docs / "up.png", y=-1)
